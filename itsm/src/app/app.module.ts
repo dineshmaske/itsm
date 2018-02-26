5f4df638cc24a6c4d1e6608da42aa6c49652b478
Dinesh <dmaske@Dineshs-MacBook-Air.local>
2018-02-27 00:11:00 +0530

    Routing added
    Added Routing model

--- itsm/src/app/app.module.ts
+++ itsm/src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
+     RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule
   ],
   providers: [],
